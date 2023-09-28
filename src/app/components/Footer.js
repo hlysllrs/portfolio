d9ad4821a98793a501bd03eee3289c0488ae2ca4
hlysllrs <hailey.sellars@gmail.com>
2023-09-28 16:18:03 -0400

    capitalize links and update email address

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -4,15 +4,15 @@ export default function Footer() {
     return (
         <footer className="footer grid grid-cols-1 md:grid-cols-12 mx-4 mb-4 text-lg font-semibold">
             <div className="md:col-span-3 flex justify-between">
-                <Link href="mailto:hailey.sellars@gmail.com">email</Link>
+                <Link href="mailto:hello@haileysellars.com">Email</Link>
                 <Link
                     href="https://www.linkedin.com/in/hailey-sellars/"
                     target="_blank"
                 >
-                    linkedin
+                    LinkedIn
                 </Link>
                 <Link href="https://github.com/hlysllrs" target="_blank">
-                    github
+                    GitHub
                 </Link>
             </div>
             <div className="md:col-start-12 justify-self-end">
